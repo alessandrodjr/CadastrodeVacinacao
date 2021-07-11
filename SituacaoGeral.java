f75869232745361ee32d528f2e8ef01f0996a6d4
MacenaKelvin <85961112+MacenaKelvin@users.noreply.github.com>
2021-07-11 11:36:13 -0300

    Pagina Situação geral

---
+++ SituacaoGeral.java
@@ -0,0 +1,16 @@
+package br.com.progvacinacao;
+
+public class SituacaoGeral {
+	
+	String dadosGerais;
+	public String retornadadosGerais()
+	{
+		return dadosGerais;
+	}
+	public static void main(String[] args) {
+
+		dadosGerais = new dadosGerais();
+		System.out.println();
+
+	}
+}
