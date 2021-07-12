c85c979f0151e67889f26e63c6f9112aa4d33a6c
MacenaKelvin <85961112+MacenaKelvin@users.noreply.github.com>
2021-07-12 12:02:02 -0300

    Update SituacaoGeral.java

--- src/br/com/progvacinacao/SituacaoGeral.java
+++ src/br/com/progvacinacao/SituacaoGeral.java
@@ -2,15 +2,9 @@ package br.com.progvacinacao;
 
 public class SituacaoGeral {
 	
-	String dadosGerais;
-	public String retornadadosGerais()
-	{
-		return dadosGerais;
-	}
-	public static void main(String[] args) {
-
-		dadosGerais = new dadosGerais();
-		System.out.println();
-
+		public  void dadosGerais() {
+		
+		System.out.println("infos.");
+	
 	}
 }
